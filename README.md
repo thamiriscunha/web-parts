bc78206e5e8c13174d86929a05da64f6c763640d
= <=>
2020-05-26 18:06:21 -0300

    Unused vars removed from carousel; Slider added;

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
-# purecarousel
-Pure and simple CSS3 - JS/jQuery carousel
+# Web Parts
+Pure and simple CSS3 - JS/jQuery web parts, like: carousel, sliders, etc...
 
-The purpose is to be simple and lightweight.
+**The purpose is to be simple and lightweight.**
 
-Use:
-  - Just add the script from main.js to your main js script;
-  - You can modify colors and layout as you wish, but .content-carousel must be flex and its width will be given by the js code;
-  - For each .content must exist a #carouselController .button;
-  - In master.css there is a 'not important stuff'. Below that mark there is just style for the github page. No need to use in your project
+## Usage:
+
+  1. The element parts styles and functionality are divided in <code>master.css</code> and <code>main.js</code> as follow:
+    1. <code>master.css</code>
+    ```css
+      /* ---------- CAROUSEL STYLE ---------- */
+      main .container #carouselController{}
+
+      /* ---------- SLIDER STYLE ---------- */
+      main .container #sliderContainer{}
+    ```
+    2. <code>main.js</code>
+    ```javascript
+      /* ---------- CAROUSEL SCRIPTS ---------- */
+      $('#carouselContainer').ready(function(){});
+
+      /* ---------- SLIDER SCRIPTS ---------- */
+      $('#sliderContainer').ready(function(){});
+    ```
+
+  2. Choose what you want and copy to your project;
+  3. Modify as needed.
--- js/main.js
+++ js/main.js
@@ -1,44 +1,81 @@
-$(document).ready(function(){
+/* ---------- CAROUSEL SCRIPTS ---------- */
+$('#carouselContainer').ready(function(){
 
-  var actualSlide = 0;
-  var totalSlides = $("#carousel .content").length;
-  var gap = 100 / totalSlides;
-  var lastSlide = $("#carousel .content").length - 1;
+  let actualSlide = 0;
+  const totalSlides = $('#carousel .content').length;
+  const gap = 100 / totalSlides;
 
-  $("#carousel").css("width", (100 * totalSlides) + "%");
-  $("#carouselController .button:eq(" + actualSlide + ")").addClass("active");
+  $('#carousel').css('width', (100 * totalSlides) + '%');
+  $('#carouselController .button:eq(' + actualSlide + ')').addClass('active');
 
-  var slide = function(){
+  let slide = function(){
 
     if(actualSlide >= totalSlides)
       actualSlide = 0;
 
-    $("#carouselController .button").removeClass("active");
-    $("#carouselController .button:eq(" + actualSlide + ")").addClass("active");
+    $('#carouselController .button').removeClass('active');
+    $('#carouselController .button:eq(' + actualSlide + ')').addClass('active');
 
-    for (var i = 0; i < totalSlides; i++){
+    for (let i = 0; i < totalSlides; i++){
 
       if(actualSlide != totalSlides)
-        $("#carousel .content:eq(" + i + ")").css(
-          "left", ((gap * actualSlide) * -1) + "%"
+        $('#carousel .content:eq(' + i + ')').css(
+          'left', ((gap * actualSlide) * -1) + '%'
         );
       else
-      $("#carousel .content:eq(" + i + ")").css(
-        "left", "0%"
+      $('#carousel .content:eq(' + i + ')').css(
+        'left', '0%'
       );
     }
   }
 
-  $("#carouselController .button").on("click", function(){
+  $('#carouselController .button').on('click', function(){
     actualSlide = $(this).index();
     slide();
   });
 
   window.setInterval(function(){
-    if(!$("#carouselContainer").is(':hover')){
+    if(!$('#carouselContainer').is(':hover')){
       actualSlide += 1;
       slide();
     }
   }, 5000);
+
+});
+
+/* ---------- SLIDER SCRIPTS ---------- */
+$('#sliderContainer').ready(function(){
+
+  let position = 0;
+  let items = $('#sliderContainer .items .item');
+
+  let containerWidth = $('#sliderContainer').width();
+  let itemsContainerWidth = $('#sliderContainer .items').width();
+
+  let moveGap = (100 / itemsContainerWidth) * containerWidth;
+
+  $('#leftBtn').on('click', function(){
+
+    if((position + moveGap) > 0){
+      position = 0;
+    } else {
+      position += moveGap;
+    }
+
+    items.css('left', position + '%');
+
+  });
+
+  $('#rightBtn').on('click', function(){
+
+    if((position - moveGap) < (-100 + moveGap)){
+      position = -100 + moveGap;
+    } else {
+      position -= moveGap;
+    }
+
+    items.css('left', position + '%');
+
+  });
 
 });
